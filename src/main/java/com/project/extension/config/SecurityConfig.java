85bd2710437d29759dd35f15388467240e0fcfc3
Eduarda Dias <maria.dias@sptech.school>
2025-08-27 15:34:16 -0300

    fix(app): Corrigindo endpoints

--- src/main/java/com/project/extension/config/SecurityConfig.java
+++ src/main/java/com/project/extension/config/SecurityConfig.java
@@ -41,9 +41,7 @@ public class SecurityConfig {
     };
 
     private static final String[] ENDPOINTS_RESTRITOS_ADMIN = {
-            "/solicitacoes/aceitar/**",
-            "/solicitacoes/recusar/**",
-            "/solicitacoes/listar-pendentes",
+            "/solicitacoes/**",
             "/usuarios/**"
     };
 
