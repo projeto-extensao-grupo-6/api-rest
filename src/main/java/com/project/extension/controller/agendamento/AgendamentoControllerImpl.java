58ec1f8efab0e3c65b2801bfa71dfd52f1bdf129
Eduarda Dias <maria.dias@sptech.school>
2025-10-15 17:42:01 -0300

    feat(app): metodo editar, criar e deletar ednereço no agnd

--- src/main/java/com/project/extension/controller/agendamento/AgendamentoControllerImpl.java
+++ src/main/java/com/project/extension/controller/agendamento/AgendamentoControllerImpl.java
@@ -3,8 +3,11 @@ package com.project.extension.controller.agendamento;
 import com.project.extension.dto.agendamento.AgendamentoMapper;
 import com.project.extension.dto.agendamento.AgendamentoRequestDto;
 import com.project.extension.dto.agendamento.AgendamentoResponseDto;
+import com.project.extension.dto.endereco.EnderecoMapper;
 import com.project.extension.entity.Agendamento;
+import com.project.extension.entity.Endereco;
 import com.project.extension.service.AgendamentoService;
+import com.project.extension.service.EnderecoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,12 +22,18 @@ public class AgendamentoControllerImpl implements AgendamentoControllerDoc{
 
     private final AgendamentoService service;
     private final AgendamentoMapper mapper;
+    private final EnderecoMapper enderecoMapper;
+    private final EnderecoService enderecoService;
 
     @Override
     public ResponseEntity<AgendamentoResponseDto> salvar(AgendamentoRequestDto request) {
-        Agendamento agendamentoSalvar = mapper.toEntity(request);
-        Agendamento agendamentoSalvo = service.salvar(agendamentoSalvar);
+        Endereco endereco = enderecoMapper.toEntity(request.Endereco());
+        Endereco enderecoSalvo = enderecoService.cadastrar(endereco);
 
+        Agendamento agendamento = mapper.toEntity(request);
+        agendamento.setEndereco(enderecoSalvo);
+
+        Agendamento agendamentoSalvo = service.salvar(agendamento);
         return ResponseEntity.status(201).body(mapper.toResponse(agendamentoSalvo));
     }
 
@@ -48,15 +57,27 @@ public class AgendamentoControllerImpl implements AgendamentoControllerDoc{
 
     @Override
     public ResponseEntity<AgendamentoResponseDto> atualizar(AgendamentoRequestDto request, Integer id) {
-        Agendamento agendamentoAtualizar = mapper.toEntity(request);
-        Agendamento agendamentoAtualizado = service.editar(agendamentoAtualizar, id);
+        Agendamento agendamentoExistente = service.buscarPorId(id);
+
+        Endereco enderecoAtualizado = enderecoMapper.toEntity(request.Endereco());
+        enderecoAtualizado.setId(agendamentoExistente.getEndereco().getId());
+        enderecoService.editar(enderecoAtualizado, enderecoAtualizado.getId());
+
+        Agendamento agendamentoAtualizado = mapper.toEntity(request);
+        agendamentoAtualizado.setEndereco(enderecoAtualizado);
+        agendamentoAtualizado = service.editar(agendamentoAtualizado, id);
 
         return ResponseEntity.status(200).body(mapper.toResponse(agendamentoAtualizado));
     }
 
     @Override
     public ResponseEntity<String> deletar(Integer id) {
+        Agendamento agendamento = service.buscarPorId(id);
+        Integer idEndereco = agendamento.getEndereco().getId();
+
         service.deletar(id);
-        return ResponseEntity.ok("Agendamento deletado com sucesso.");
+        enderecoService.deletar(idEndereco);
+
+        return ResponseEntity.ok("Agendamento e endereço deletados com sucesso.");
     }
 }
