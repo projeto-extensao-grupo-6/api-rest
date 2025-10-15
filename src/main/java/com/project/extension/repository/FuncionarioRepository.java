34e6d37d017a6476cc933c2cf197ccd3e8a680d4
Eduarda Dias <maria.dias@sptech.school>
2025-10-15 18:55:22 -0300

    feat(app): criando repository de funcionario

---
+++ src/main/java/com/project/extension/repository/FuncionarioRepository.java
@@ -0,0 +1,7 @@
+package com.project.extension.repository;
+
+import com.project.extension.entity.Funcionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
+}
