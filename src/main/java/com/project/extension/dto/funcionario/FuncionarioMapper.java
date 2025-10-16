4df42bcd78bb932fd52dbc8bd244940f7f899018
Eduarda Dias <maria.dias@sptech.school>
2025-10-16 16:49:02 -0300

    feat(app): criando mapper para funcionario

--- src/main/java/com/project/extension/dto/funcionario/FuncionarioMapper.java
+++ src/main/java/com/project/extension/dto/funcionario/FuncionarioMapper.java
@@ -3,6 +3,9 @@ package com.project.extension.dto.funcionario;
 import com.project.extension.entity.Funcionario;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
+import java.util.List;
+
 @Component
 public class FuncionarioMapper {
 
@@ -18,6 +21,22 @@ public class FuncionarioMapper {
         );
     }
 
+    public List<Funcionario> toEntity(List<FuncionarioRequestDto> dtos) {
+        if (dtos == null || dtos.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return dtos.stream()
+                .map(dto -> new Funcionario(
+                        dto.nome(),
+                        dto.telefone(),
+                        dto.funcao(),
+                        dto.contrato(),
+                        dto.ativo() != null ? dto.ativo() : true
+                ))
+                .toList();
+    }
+
     public FuncionarioResponseDto toResponse(Funcionario funcionario) {
         if (funcionario == null) return null;
 
@@ -29,5 +48,22 @@ public class FuncionarioMapper {
                 funcionario.getContrato(),
                 funcionario.getAtivo()
         );
+    }
+
+    public List<FuncionarioResponseDto> toResponse(List<Funcionario> funcionarios) {
+        if (funcionarios == null || funcionarios.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return funcionarios.stream()
+                .map(funcionario -> new FuncionarioResponseDto(
+                        funcionario.getId(),
+                        funcionario.getNome(),
+                        funcionario.getTelefone(),
+                        funcionario.getFuncao(),
+                        funcionario.getContrato(),
+                        funcionario.getAtivo()
+                ))
+                .toList();
     }
 }
