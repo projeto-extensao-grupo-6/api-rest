bb08c04a1799899aeba8b3683dc7bb448e14993f
Eduarda Dias <maria.dias@sptech.school>
2025-10-16 16:48:45 -0300

    feat(app): criando metodo no reposiroty endereco buscacep

--- src/main/java/com/project/extension/repository/EnderecoRepository.java
+++ src/main/java/com/project/extension/repository/EnderecoRepository.java
@@ -4,4 +4,5 @@ import com.project.extension.entity.Endereco;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+    Endereco findByCep(String cep);
 }
