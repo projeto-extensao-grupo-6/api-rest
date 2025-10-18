f816f8ef37d60c96d54824492a3453b63b68413e
Eduarda Dias <maria.dias@sptech.school>
2025-10-18 00:42:23 -0300

    refactor(app): refatorando entity historico

--- src/main/java/com/project/extension/entity/HistoricoEstoque.java
+++ src/main/java/com/project/extension/entity/HistoricoEstoque.java
@@ -1,27 +1,32 @@
 package com.project.extension.entity;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name ="historico_estoque")
+@Getter
+@Setter
+@NoArgsConstructor
 public class HistoricoEstoque {
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "fk_estoque", nullable = false)
-    private Estoque estoque; // Mapeia a fk_estoque
+    @JoinColumn(name = "estoque_id", nullable = false)
+    private Estoque estoque;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "fk_usuario")
-    private Usuario usuario; // Mapeia fk_usuario (Você precisaria da Entity Usuario)
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "tipo_movimentacao", nullable = false)
-    private TipoMovimentacao tipoMovimentacao; // ENUM: ENTRADA ou SAIDA
+    private TipoMovimentacao tipoMovimentacao;
 
     @Column(nullable = false)
     private Integer quantidade;
@@ -31,63 +36,4 @@ public class HistoricoEstoque {
 
     @Column(name = "data_movimentacao")
     private LocalDateTime dataMovimentacao;
-
-    // Construtor, getters e setters
-    public HistoricoEstoque() {}
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Estoque getEstoque() {
-        return estoque;
-    }
-
-    public void setEstoque(Estoque estoque) {
-        this.estoque = estoque;
-    }
-
-    public Usuario getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(Usuario usuario) {
-        this.usuario = usuario;
-    }
-
-    public TipoMovimentacao getTipoMovimentacao() {
-        return tipoMovimentacao;
-    }
-
-    public void setTipoMovimentacao(TipoMovimentacao tipoMovimentacao) {
-        this.tipoMovimentacao = tipoMovimentacao;
-    }
-
-    public Integer getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(Integer quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public String getObservacao() {
-        return observacao;
-    }
-
-    public void setObservacao(String observacao) {
-        this.observacao = observacao;
-    }
-
-    public LocalDateTime getDataMovimentacao() {
-        return dataMovimentacao;
-    }
-
-    public void setDataMovimentacao(LocalDateTime dataMovimentacao) {
-        this.dataMovimentacao = dataMovimentacao;
-    }
 }
