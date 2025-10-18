c62f0c51adf99756d311e7cb4376af6977d8e79f
Eduarda Dias <maria.dias@sptech.school>
2025-10-18 00:41:25 -0300

    refactor(app): adicionando atributos para produto

--- src/main/resources/db/migration/v202516101110__create_tables_estoque.sql
+++ src/main/resources/db/migration/v202516101110__create_tables_estoque.sql
@@ -2,6 +2,8 @@ CREATE TABLE produto (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     descricao VARCHAR(255),
+    unidade_medida VARCHAR(255),
+    preco DECIMAL(16, 5),
     ativo BOOLEAN DEFAULT TRUE COMMENT 'Define se o produto está ativo no catálogo',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Data de criação do registro',
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Data de atualização do registro'
@@ -9,33 +11,61 @@ CREATE TABLE produto (
 
 CREATE TABLE atributo_produto (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_produto INT NOT NULL,
+    produto_id INT NOT NULL,
     tipo VARCHAR(100) COMMENT 'Tipo de atributo (Ex: cor, espessura, material)',
     valor VARCHAR(100) COMMENT 'Valor do atributo (Ex: verde, 8mm, alumínio)',
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Data de criação do registro',
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Data de atualização do registro',
-    FOREIGN KEY (id_produto) REFERENCES produto(id)
+    FOREIGN KEY (produto_id) REFERENCES produto(id)
 );
 
 CREATE TABLE estoque (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_produto INT NOT NULL,
+    produto_id INT NOT NULL,
     quantidade INT CHECK (quantidade >= 0),
     reservado INT DEFAULT 0,
     localizacao VARCHAR(100),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Data de entrada no estoque',
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Data de atualização',
-    FOREIGN KEY (id_produto) REFERENCES produto(id)
+    FOREIGN KEY (produto_id) REFERENCES produto(id)
 );
 
 CREATE TABLE historico_estoque (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_estoque INT NOT NULL,
-    id_usuario INT,
+    estoque_id INT NOT NULL,
+    usuario_id INT,
     tipo_movimentacao ENUM('ENTRADA','SAIDA') NOT NULL COMMENT 'Tipo de movimentação',
     quantidade INT NOT NULL,
     observacao VARCHAR(255),
     data_movimentacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP COMMENT 'Data e hora da movimentação',
-    FOREIGN KEY (id_estoque) REFERENCES estoque(id),
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id)
+    FOREIGN KEY (estoque_id) REFERENCES estoque(id),
+    FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
+
+CREATE TABLE metrica_estoque (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    produto_id INT NOT NULL,
+    nivel_minimo INT DEFAULT 0,
+    nivel_maximo INT,
+    FOREIGN KEY (produto_id) REFERENCES produto(id)
+);
+
+
+CREATE TRIGGER trg_estoque_after_update
+AFTER UPDATE ON estoque
+FOR EACH ROW
+BEGIN
+    DECLARE tipo_mov VARCHAR(10);
+
+    IF NEW.quantidade > OLD.quantidade THEN
+        SET tipo_mov = 'ENTRADA';
+    ELSEIF NEW.quantidade < OLD.quantidade THEN
+        SET tipo_mov = 'SAIDA';
+    END IF;
+
+    IF tipo_mov IS NOT NULL THEN
+        INSERT INTO historico_estoque (estoque_id, usuario_id, tipo_movimentacao, quantidade, observacao)
+        VALUES (NEW.id, NULL, tipo_mov, ABS(NEW.quantidade - OLD.quantidade), 'Atualização automática de estoque');
+    END IF;
+END;
+
