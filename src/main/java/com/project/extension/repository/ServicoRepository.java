f75f5139fa6944ff85a50e53f528f03ae9bf2eb7
Eduarda Dias <maria.dias@sptech.school>
2025-10-18 00:40:50 -0300

    refactor(app): retirando classes nao usadas

--- src/main/java/com/project/extension/repository/ServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.extension.repository;
-
-import com.project.extension.entity.Servico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ServicoRepository extends JpaRepository<Servico, Integer> {
-}
