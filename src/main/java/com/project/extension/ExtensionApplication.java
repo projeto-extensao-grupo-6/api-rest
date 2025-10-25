1fc65208af607478a02604bdceace3440bbc0b8f
Eduarda Dias <maria.dias@sptech.school>
2025-10-25 10:34:59 -0300

    feat(app): criando todo

--- src/main/java/com/project/extension/ExtensionApplication.java
+++ src/main/java/com/project/extension/ExtensionApplication.java
@@ -8,5 +8,20 @@ public class ExtensionApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ExtensionApplication.class, args);
+
+//        TODO:
+//
+//        SPRINT 2:
+//
+//        - padrão de projeto strategy
+//        - relacionar cliente-pedido-agendamento-endereco-funcionario-estoqu
+//        - relacionar pedido com cliente
+//
+//        SPRINT 3:
+//
+//        - endpoint para mudar senha de usuario no primeiro login
+//        - microserviço para ler excel
+//        - endpoints personalizados
+
     }
 }
