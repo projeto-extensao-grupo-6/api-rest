c20db85879ca712fc08147227033d61c2b9334fd
Eduarda Dias <maria.dias@sptech.school>
2025-11-16 14:21:23 -0300

    feat(app): create repository fro metrica de estoque

---
+++ src/main/java/com/project/extension/repository/MetricaRepository.java
@@ -0,0 +1,7 @@
+package com.project.extension.repository;
+
+import com.project.extension.entity.Metrica;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MetricaRepository extends JpaRepository<Metrica, Integer> {
+}
