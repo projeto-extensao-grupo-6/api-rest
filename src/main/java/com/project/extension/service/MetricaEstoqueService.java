40f57d0a1eebfe9d200380da87199eae678553ef
Eduarda Dias <maria.dias@sptech.school>
2025-11-16 17:42:55 -0300

    feat(app): ajustando service para novo relacionamento

--- src/main/java/com/project/extension/service/MetricaEstoqueService.java
+++ src/main/java/com/project/extension/service/MetricaEstoqueService.java
@@ -1,9 +1,9 @@
 package com.project.extension.service;
 
-import com.project.extension.entity.Metrica;
+import com.project.extension.entity.MetricaEstoque;
 import com.project.extension.entity.Produto;
 import com.project.extension.exception.naoencontrado.MetricaNaoEncontradaException;
-import com.project.extension.repository.MetricaRepository;
+import com.project.extension.repository.MetricaEstoqueRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,38 +13,36 @@ import java.util.List;
 @Service
 @Slf4j
 @AllArgsConstructor
-public class MetricaService {
+public class MetricaEstoqueService {
 
-    private final MetricaRepository repository;
+    private final MetricaEstoqueRepository repository;
 
-    public Metrica cadastrar(Metrica metrica, Produto produto) {
-        metrica.setProduto(produto);
-
-        Metrica metricaSalvo = repository.save(metrica);
+    public MetricaEstoque cadastrar(MetricaEstoque metricaEstoque) {
+        MetricaEstoque metricaEstoqueSalvo = repository.save(metricaEstoque);
         log.info("Métrica de estoque salvo com sucesso!");
-        return metricaSalvo;
+        return metricaEstoqueSalvo;
     }
 
 
-    public Metrica buscarPorId(Integer id) {
+    public MetricaEstoque buscarPorId(Integer id) {
         return repository.findById(id).orElseThrow(() -> {
             log.error("Métrica de estoque com ID " + id + " não encontrado");
             return new MetricaNaoEncontradaException();
         });
     }
 
-    public List<Metrica> listar() {
-        List<Metrica> metricas = repository.findAll();
-        log.info("Total de métricas de estoque encontradas: " + metricas.size());
-        return metricas;
+    public List<MetricaEstoque> listar() {
+        List<MetricaEstoque> metricaEstoques = repository.findAll();
+        log.info("Total de métricas de estoque encontradas: " + metricaEstoques.size());
+        return metricaEstoques;
     }
 
-    public Metrica editar(Metrica origem, Integer id) {
-        Metrica destino = this.buscarPorId(id);
+    public MetricaEstoque editar(MetricaEstoque origem, Integer id) {
+        MetricaEstoque destino = this.buscarPorId(id);
 
         this.atualizarDadosBasicos(destino, origem);
 
-        Metrica produtoAtualizado = repository.save(destino);
+        MetricaEstoque produtoAtualizado = repository.save(destino);
         log.info("Atributo Produto atualizado com sucesso!");
         return produtoAtualizado;
     }
@@ -54,7 +52,7 @@ public class MetricaService {
         log.info("Atributo Produto deletado com sucesso");
     }
 
-    private void atualizarDadosBasicos(Metrica destino, Metrica origem) {
+    private void atualizarDadosBasicos(MetricaEstoque destino, MetricaEstoque origem) {
         destino.setNivelMinimo(origem.getNivelMinimo());
         destino.setNivelMaximo(origem.getNivelMaximo());
     }
