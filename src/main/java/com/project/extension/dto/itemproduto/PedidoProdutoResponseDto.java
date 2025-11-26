a97944df71fac4fabfccdc7c1b0d34e36d60e113
Phelipe <phelipe.silva@sptech.school>
2025-11-26 00:53:55 -0300

    feat(app): Criando dtos de response, request e mapper

---
+++ src/main/java/com/project/extension/dto/itemproduto/PedidoProdutoResponseDto.java
@@ -0,0 +1,18 @@
+package com.project.extension.dto.itemproduto;
+
+import com.project.extension.dto.cliente.ClienteResponseDto;
+import com.project.extension.dto.status.StatusResponseDto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public record PedidoProdutoResponseDto(
+        Integer id,
+        ClienteResponseDto cliente,
+        StatusResponseDto status,
+        BigDecimal valorTotal,
+        String observacao,
+        List<ItemPedidoResponseDto> itensPedido
+) {
+}
