7365462ddc3f8b05fde7880dae1e486ed2bd3fb7
Eduarda Dias <maria.dias@sptech.school>
2025-11-30 21:53:19 -0300

    create pedido mapper

--- src/main/java/com/project/extension/dto/pedido/PedidoMapper.java
+++ src/main/java/com/project/extension/dto/pedido/PedidoMapper.java
@@ -1,10 +1,16 @@
 package com.project.extension.dto.pedido;
 
 import com.project.extension.dto.cliente.ClienteMapper;
+import com.project.extension.dto.pedido.produto.ItemPedidoMapper;
+import com.project.extension.dto.pedido.produto.ItemPedidoRequestDto;
+import com.project.extension.dto.pedido.servico.ServicoMapper;
+import com.project.extension.dto.pedido.servico.ServicoRequestDto;
 import com.project.extension.dto.status.StatusMapper;
 import com.project.extension.entity.Pedido;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -12,45 +18,55 @@ public class PedidoMapper {
 
     private final StatusMapper statusMapper;
     private final ClienteMapper clienteMapper;
+    private final ServicoMapper servicoMapper;
+    private final ItemPedidoMapper produtoMapper;
 
-    public Pedido toEntity(PedidoRequestDto dto) {
-        if (dto == null) return null;
+    public Pedido toEntity(PedidoRequestDto request) {
+        if (request == null) return null;
 
-        Pedido pedido =  new Pedido(
-                dto.valorTotal(),
-                dto.ativo(),
-                dto.observacao(),
-                dto.formaPagamento(),
-                dto.tipoPedido()
-        );
+        Pedido pedido = fromBase(request.pedido());
 
-        pedido.setStatus(statusMapper.toEntity(dto.status()));
-        pedido.setCliente(clienteMapper.toEntity(dto.cliente()));
+        if (request.servico() != null) {
+            return mapServico(request.servico(), pedido);
+        }
+
+        if (request.produtos() != null) {
+            return mapProduto(request.produtos(), pedido);
+        }
+
+        throw new IllegalArgumentException("Estrutura inválida: pedido precisa ser SERVICO ou PRODUTO.");
+    }
+
+    private Pedido mapServico(ServicoRequestDto dto, Pedido pedido) {
+        pedido.setTipoPedido("serviço");
+
+        var servicoEntity = servicoMapper.toEntity(dto);
+
+        servicoEntity.setPedido(pedido);
+
+        return pedido;
+    }
+    private Pedido mapProduto(List<ItemPedidoRequestDto> itensDto, Pedido pedido) {
+        pedido.setTipoPedido("produto");
+
+        var itens = itensDto.stream()
+                .map(itemDto -> produtoMapper.toEntity(itemDto, pedido))
+                .toList();
+
+        pedido.setItensPedido(itens);
+
         return pedido;
     }
 
-    public Pedido toEntity(PedidoResponseDto dto) {
-        if (dto == null) return null;
-
-        Pedido pedido = new Pedido(
-                dto.valorTotal(),
-                dto.ativo(),
-                dto.observacao(),
-                dto.formaPagamento(),
-                dto.tipoPedido()
-        );
-
-        pedido.setId(dto.id());
-
-        if (dto.status() != null) {
-            pedido.setStatus(statusMapper.toEntity(dto.status()));
-        }
-
-        if (dto.cliente() != null){
-            pedido.setCliente(clienteMapper.toEntity(dto.cliente()));
-        }
-
-        return pedido;
+    private Pedido fromBase(PedidoBaseRequestDto dto) {
+        Pedido p = new Pedido();
+        p.setValorTotal(dto.valorTotal());
+        p.setAtivo(dto.ativo());
+        p.setObservacao(dto.observacao());
+        p.setFormaPagamento(dto.formaPagamento());
+        p.setCliente(clienteMapper.toEntity(dto.cliente()));
+        p.setStatus(statusMapper.toEntity(dto.status()));
+        return p;
     }
 
     public PedidoResponseDto toResponse(Pedido pedido) {
@@ -64,7 +80,11 @@ public class PedidoMapper {
                 pedido.getFormaPagamento(),
                 pedido.getTipoPedido(),
                 clienteMapper.toResponse(pedido.getCliente()),
-                statusMapper.toResponse(pedido.getStatus())
+                statusMapper.toResponse(pedido.getStatus()),
+                pedido.getItensPedido().stream()
+                        .map(produtoMapper::toResponse)
+                        .toList(),
+                servicoMapper.toResponse(pedido.getServico())
         );
     }
 }
