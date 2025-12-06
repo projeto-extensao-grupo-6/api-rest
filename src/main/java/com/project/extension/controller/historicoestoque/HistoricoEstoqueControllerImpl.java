20ae7c5f87151b2b3455c7ab4b79f0e1b147f58a
Eduarda Dias <maria.dias@sptech.school>
2025-12-06 20:03:52 -0300

    refactor(app): ajustando imports

--- src/main/java/com/project/extension/controller/historicoestoque/HistoricoEstoqueControllerImpl.java
+++ src/main/java/com/project/extension/controller/historicoestoque/HistoricoEstoqueControllerImpl.java
@@ -22,7 +22,6 @@ public class HistoricoEstoqueControllerImpl implements HistoricoEstoqueControlle
     @Override
     public ResponseEntity<List<HistoricoEstoqueResponseDto>> listar() {
         List<HistoricoEstoque> historicoEstoques = service.listar();
-
         return historicoEstoques.isEmpty()
                 ? ResponseEntity.status(204).build()
                 : ResponseEntity.status(200).body(historicoEstoques.stream()
@@ -32,7 +31,8 @@ public class HistoricoEstoqueControllerImpl implements HistoricoEstoqueControlle
 
     @Override
     public ResponseEntity<List<HistoricoEstoqueResponseDto>> buscarPorId(Integer id) {
-       List<HistoricoEstoque> historicoEstoques = service.buscarPorEstoqueId(id);
+        List<HistoricoEstoque> historicoEstoques = service.buscarPorEstoqueId(id);
+
         return historicoEstoques.isEmpty()
                 ? ResponseEntity.status(204).build()
                 : ResponseEntity.status(200).body(historicoEstoques.stream()
